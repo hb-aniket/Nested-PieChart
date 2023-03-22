98b662547576427cd5d1ef5486a00f466d63cc36
hb-aniket <126682911+hb-aniket@users.noreply.github.com>
2023-03-22 15:30:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import NestedChart from './componenets/NestedChart';
+import data from './componenets/data'
+function App() {
+  return (
+    <div className="App">
+      <NestedChart data={data}  />
+    </div>
+  );
+}
+export default App;
